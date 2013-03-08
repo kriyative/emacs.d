7566d5dc431c004c0c9a233b8c3aab7fc1b44b5e
Ram Krishnan <kriyative@gmail.com>
2013-03-07 20:28:28 -0800

    Pull package info on first start, and updated install scripts for non-packages dependencies.

--- bootstrap/deps.el
+++ bootstrap/deps.el
@@ -27,30 +27,47 @@
 	     '("marmalade" . "http://marmalade-repo.org/packages/")
 	     t)
 (package-initialize)
+(unless (file-directory-p (expand-file-name "~/.emacs.d/elpa/archives"))
+  (package-refresh-contents))
 
 (dolist (pkg packages-deps)
   (unless (package-installed-p pkg)
     (package-install pkg)))
 
 (defvar non-packages-deps
-  '(("emacs-w3m" :src "http://dl.dropbox.com/u/64576/share/sw/emacs-w3m.tar.gz")))
+  '(("emacs-w3m" :src "http://dl.dropbox.com/u/64576/share/sw/emacs-w3m.tar.gz")
+    ("clojure-emacs-hacks" :src "git@github.com:kriyative/clojure-emacs-hacks.git")))
+
+(defun download-and-install (src tmpdir fname)
+  (let ((cmd (cond
+	      ((string-match "^http" src)
+	       (lambda ()
+		 (eshell-command
+		  (format "cd /tmp/%s; curl -O %s; cd ~/.emacs.d; tar xzf %s"
+			  tmpdir
+			  src
+			  (concat "/tmp/" tmpdir "/" fname)))))
+	      ((string-match "^git" src)
+	       (lambda ()
+		 (eshell-command
+		  (format "cd ~/.emacs.d; git clone %s" src))))
+	      (t (lambda ()
+		   (message "Don't know how to download/install %s" src))))))
+    (funcall cmd)))
 
 (let ((tmpdir (make-temp-name "emacs-bootstrap-")))
-  (dolist (dep non-packages-deps)
-    (let* ((key (car dep))
-	   (plist (cdr dep))
-	   (src (getf plist :src))
-	   (url (url-generic-parse-url src))
-	   (fname (file-name-nondirectory (url-filename url)))
-	   (dir (expand-file-name (concat "~/.emacs.d/" key))))
-      (unless (file-directory-p dir)
-	(message "downloading and unpacking %s ..." key)
-	(eshell-command (format "mkdir -p /tmp/%s" tmpdir))
-	(eshell-command (format "cd /tmp/%s; curl -O %s" tmpdir src))
-	(eshell-command (format "cd %s; tar xzf /tmp/%s/%s"
-				(expand-file-name "~/.emacs.d/")
-				tmpdir
-				fname)))
-      (add-to-list 'load-path dir)))
-  (eshell-command (format "rm -fr /tmp/%s" tmpdir)))
-
+  (eshell-command (format "mkdir -p /tmp/%s" tmpdir))
+  (unwind-protect
+      (dolist (dep non-packages-deps)
+	(let* ((key (car dep))
+	       (plist (cdr dep))
+	       (src (getf plist :src))
+	       (url (url-generic-parse-url src))
+	       (fname (file-name-nondirectory (url-filename url)))
+	       (dir (expand-file-name (concat "~/.emacs.d/" key))))
+	  (unless (file-directory-p dir)
+	    (message "downloading and unpacking %s ..." key)
+	    (eshell-command (format "mkdir -p /tmp/%s" tmpdir))
+	    (download-and-install src tmpdir fname))
+	  (add-to-list 'load-path dir)))
+    (eshell-command (format "rm -fr /tmp/%s" tmpdir))))
