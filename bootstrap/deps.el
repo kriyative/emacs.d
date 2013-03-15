226e06148164d911ecc66859b50830c2ec14207c
Ram Krishnan <kriyative@gmail.com>
2013-03-14 21:29:22 -0700

    The download/install logic no longer requires eshell.

--- bootstrap/deps.el
+++ bootstrap/deps.el
@@ -1,8 +1,8 @@
-(require 'eshell)
+(require 'cl)
 (require 'url)
 
 (defun emacs22-package-init ()
-  (let ((elpa-package-el (expand-file-name "~/.emacs.d/elpa/package.el")))
+  (let ((elpa-package-el (expand-file-name "~/.emacs.d/elpa22/package.el")))
     (if (file-exists-p elpa-package-el)
         (load elpa-package-el)
         (let ((buffer (url-retrieve-synchronously
@@ -65,37 +65,84 @@
 
 (defvar non-packages-deps
   '(("emacs-w3m" :src "http://dl.dropbox.com/u/64576/share/sw/emacs-w3m.tar.gz")
-    ("clojure-emacs-hacks" :src "git@github.com:kriyative/clojure-emacs-hacks.git")))
+    ("clojure-emacs-hacks" :src "https://github.com/kriyative/clojure-emacs-hacks.git")))
 
-(defun download-and-install (src tmpdir fname)
-  (let ((cmd (cond
-	      ((string-match "^http" src)
-	       (lambda ()
-		 (eshell-command
-		  (format "cd /tmp/%s; curl -O %s; cd ~/.emacs.d; tar xzf %s"
-			  tmpdir
-			  src
-			  (concat "/tmp/" tmpdir "/" fname)))))
-	      ((string-match "^git" src)
-	       (lambda ()
-		 (eshell-command
-		  (format "cd ~/.emacs.d; git clone %s" src))))
-	      (t (lambda ()
-		   (message "Don't know how to download/install %s" src))))))
-    (funcall cmd)))
+(defmacro with-cwd (dir &rest body)
+  (let ((cwd% (gensym)))
+    `(let ((,cwd% default-directory))
+       (unwind-protect
+           (progn
+             (cd-absolute ,dir)
+             ,@body)
+         (cd-absolute ,cwd%)))))
 
-(let ((tmpdir (make-temp-name "emacs-bootstrap-")))
-  (eshell-command (format "mkdir -p /tmp/%s" tmpdir))
+(defvar init-dir "~/.emacs.d")
+
+(defun test-string-match (item x) (string-match x item))
+
+(defvar unpackers-alist
+  `(("\\(tgz\\|tar\.gz\\)$"
+     ,(lambda (filename)
+        (call-process "tar" nil nil nil "xzf" filename)))
+    ("\\(tbz\\|tar\.bz\\|tar\.bz2\\)$"
+     ,(lambda (filename)
+        (call-process "tar" nil nil nil "xjf" filename)))
+    ("tar$"
+     ,(lambda (filename)
+        (call-process "tar" nil nil nil "xf" filename)))
+    ("zip$"
+     ,(lambda (filename)
+        (call-process "unzip" nil nil nil filename)))))
+
+(defun find-unpacker (filename)
+  (when-let (unpacker (assoc* filename unpackers-alist :test 'test-string-match))
+    (cadr unpacker)))
+
+;; (find-unpacker "emacs-w3m.tar.gz")
+;; (find-unpacker "master.zip")
+
+(defun unpack (filename)
+  (when-let (unpacker (find-unpacker filename))
+    (funcall unpacker filename)))
+
+(defun download-and-install-http (src tmpdir fname)
+  (with-cwd tmpdir
+    (call-process "curl" nil nil nil "-L" "-O" src))
+  (with-cwd init-dir
+    (unpack (concat (file-name-as-directory tmpdir) fname))))
+
+(defun download-and-install-git (src tmpdir fname)
+  (with-cwd init-dir
+    (call-process "git" nil nil nil "clone" src)))
+
+(defvar downloaders-alist
+  '(("github\.com" download-and-install-git)
+    ("^http" download-and-install-http)))
+
+(defun find-downloader (src)
+  (when-let (downloader (assoc* src downloaders-alist :test 'test-string-match))
+    (cadr downloader)))
+
+(defun download-and-install (src &rest args)
+  (if-let (downloader (find-downloader src))
+      (apply downloader src args)
+    (message "Don't know how to download/install %s" src)))
+
+(let* ((tmpname (make-temp-name "emacs-bootstrap-"))
+       (tmpdir (format "/tmp/%s" tmpname)))
   (unwind-protect
-      (dolist (dep non-packages-deps)
-	(let* ((key (car dep))
-	       (plist (cdr dep))
-	       (src (getf plist :src))
-	       (url (url-generic-parse-url src))
-	       (fname (file-name-nondirectory (url-filename url)))
-	       (dir (expand-file-name (concat "~/.emacs.d/" key))))
-	  (unless (file-directory-p dir)
-	    (message "downloading and unpacking %s ..." key)
-	    (download-and-install src tmpdir fname))
-	  (add-to-list 'load-path dir)))
-    (eshell-command (format "rm -fr /tmp/%s" tmpdir))))
+      (progn
+        (make-directory tmpdir t)
+        (add-to-list 'exec-path "/usr/local/bin")
+        (dolist (dep non-packages-deps)
+          (let* ((key (car dep))
+                 (plist (cdr dep))
+                 (src (getf plist :src))
+                 (url (url-generic-parse-url src))
+                 (fname (file-name-nondirectory (url-filename url)))
+                 (dir (expand-file-name (concat "~/.emacs.d/" key))))
+            (unless (file-directory-p dir)
+              (message "downloading and unpacking %s ..." key)
+              (download-and-install src tmpdir fname))
+            (add-to-list 'load-path dir))))
+    (delete-directory tmpdir t)))
