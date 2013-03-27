4cba08bdb24c2eacb4cbd80b049ff767a8e1df8b
Ram Krishnan <kriyative@gmail.com>
2013-03-27 00:24:12 -0700

    Load the miagi mail client, and disallow saving ~/.authinfo files.

--- init.el
+++ init.el
@@ -41,6 +41,8 @@
 (require 'clojurescript-mode)
 (require 'adoc-mode)
 
+(require 'miagi)
+
 (display-time)
 (appt-activate 1)
 (winner-mode 1)
@@ -57,7 +59,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(auth-source-save-behavior nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
