08337d66cb6d4f5a46ea324f96032314108c2733
Ram Krishnan <kriyative@gmail.com>
2013-05-14 23:27:58 -0700

    Custom face and var settings.

--- init.el
+++ init.el
@@ -61,10 +61,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(auth-source-save-behavior nil))
+ '(auth-source-save-behavior nil)
+ '(smtpmail-smtp-server "smtp.gmail.com")
+ '(smtpmail-smtp-service 25))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(fringe ((t nil))))
+ '(fringe ((t nil)))
+ '(jabber-title-medium ((t (:height 1.5)))))
