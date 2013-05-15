d35093e1c759f357e1760ffaf84ec101ddd0de81
Ram Krishnan <kriyative@gmail.com>
2013-05-14 23:26:57 -0700

    Defer changing Info face properties until info-mode is loaded.

--- bootstrap/tty.el
+++ bootstrap/tty.el
@@ -1,6 +1,11 @@
-(set-face-foreground 'Info-title-1-face "black")
-(set-face-foreground 'Info-title-2-face "black")
-(set-face-foreground 'info-xref nil)
+(defun setup-info-faces ()
+  (set-face-foreground 'Info-title-1-face "black")
+  (set-face-foreground 'Info-title-2-face "black")
+  (set-face-foreground 'info-xref nil))
+
+(eval-after-load 'info
+  '(setup-info-faces))
+
 (set-terminal-coding-system 'unix)
 (mapcar '(lambda (face)
            (set-face-background face "white")
