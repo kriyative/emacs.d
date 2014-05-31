b69f97ee488647e6f327e77c37e15736260e6cb1
Ram Krishnan <kriyative@gmail.com>
2014-05-30 17:29:52 -0700

    Added ns-fill-display alternative to ns-toggle-fullscreen.

--- bootstrap/ns.el
+++ bootstrap/ns.el
@@ -1,2 +1,12 @@
 (load-relative "mac.el")
-(define-key ctl-z-map "x" 'ns-toggle-fullscreen)
+
+(defun ns-fill-display ()
+  (interactive)
+  (setq ns-auto-hide-menu-bar t)
+  (set-frame-position (selected-frame) 0 0)
+  (set-mouse-position (selected-frame) 153 -2)
+  (when (= 1200 (display-pixel-height))
+    (set-frame-width (selected-frame) 272)
+    (set-frame-height (selected-frame) 84)))
+
+(define-key ctl-z-map "x" 'ns-fill-display)
