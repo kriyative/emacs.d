2f90a364011b0dbcdd4e31257e871c44cbe6c022
Ram Krishnan <kriyative@gmail.com>
2015-02-27 14:42:15 -0800

    X11 specific customizations.

---
+++ bootstrap/x.el
@@ -0,0 +1,6 @@
+(scroll-bar-mode -1)
+(menu-bar-mode -1)
+(global-set-key '[C-tab] 'other-window)
+
+;; (set-default-font "-unknown-DejaVu Sans Mono-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1")
+(set-default-font "-unknown-Liberation Mono-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1")
