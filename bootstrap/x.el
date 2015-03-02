43ee710613838607f3c2ec5ca2d3e492d1a56b00
Ram Krishnan <kriyative@gmail.com>
2015-03-02 11:34:36 -0800

    Use xft font name.

--- bootstrap/x.el
+++ bootstrap/x.el
@@ -3,6 +3,7 @@
 (global-set-key '[C-tab] 'other-window)
 
 ;; (set-default-font "-unknown-DejaVu Sans Mono-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1")
-(set-default-font "-unknown-Liberation Mono-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1")
+;; (set-default-font "-unknown-Liberation Mono-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1")
+(set-default-font "Liberation Mono")
 
 (try-require 'ss)
