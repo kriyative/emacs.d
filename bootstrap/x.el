45436f7adc3ef4a3cc13746995475e358ca8c23d
Ram Krishnan <kriyative@gmail.com>
2015-03-04 08:15:32 -0800

    Fixup font, and add D-Bus alerting.

--- bootstrap/x.el
+++ bootstrap/x.el
@@ -1,9 +1,36 @@
+(require 'alert)
+
 (scroll-bar-mode -1)
 (menu-bar-mode -1)
 (global-set-key '[C-tab] 'other-window)
 
-;; (set-default-font "-unknown-DejaVu Sans Mono-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1")
-;; (set-default-font "-unknown-Liberation Mono-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1")
-(set-default-font "Liberation Mono")
+;;........1.........2.........3.........4.........5.........6.........7.........8.........9
+;;23456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
+(defvar x-font nil)
+(setq x-font "Liberation Mono 9")
+;; (setq x-font "-unknown-DejaVu Sans Mono-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1")
+;; (setq x-font "-unknown-Liberation Mono-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1")
+(set-frame-font x-font t t)
+(setq default-frame-alist `((font . ,x-font)))
 
 (try-require 'ss)
+
+(global-set-key '[C-z space] 'emms-pause)
+
+(require 'emms-setup)
+(emms-all)
+(emms-default-players)
+
+(defun frame-visible? (&optional frame)
+  (eq t (cdr (assoc 'visibility (frame-parameters)))))
+
+(defun x-notify (message &optional title)
+  (let ((alert-default-style (if (frame-visible?) 'message 'notifications)))
+    (alert message :title (or title (concat "emacs@" system-name)))))
+
+;; (x-notify "hello")
+
+(defun compilation-end-notifier (buffer status)
+  (x-notify status))
+
+(push 'compilation-end-notifier compilation-finish-functions)
