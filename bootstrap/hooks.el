adc6fee383d2f2a6bddbceaf780526e898123c96
Ram Krishnan <kriyative@gmail.com>
2015-04-19 20:00:45 -0700

    Suppress magit warning message.

--- bootstrap/hooks.el
+++ bootstrap/hooks.el
@@ -155,7 +155,8 @@
   (set-face-attribute 'magit-item-highlight nil
                       :background "lightgrey"
                       :foreground "black")
-  (set-face-attribute 'magit-tag nil :foreground "black"))
+  (set-face-attribute 'magit-tag nil :foreground "black")
+  (setq magit-last-seen-setup-instructions "1.4.0"))
 
 (eval-after-load 'magit
   '(setup-magit))
