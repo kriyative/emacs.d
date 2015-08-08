ba0d70ac6ae6abc00421f040d6809278515a7973
Ram Krishnan <kriyative@gmail.com>
2015-08-07 18:31:20 -0700

    Name changes to adhere to Emacs norm.

--- packages/pwcrypt.el
+++ packages/pwcrypt.el
@@ -10,7 +10,7 @@
      (goto-char (point-min))
      ,@body))
 
-(defun pwcrypt/find-all (name)
+(defun pwcrypt-find-all (name)
   (with-pwcrypt pwcrypt-file
     (let ((match-points '()))
       (while (re-search-forward (concat "^**[ ]*" "\\(" name ".*$\\)") nil t)
@@ -19,13 +19,13 @@
           (setq match-points (cons (cons match-str beg) match-points))))
       (reverse match-points))))
 
-;; (pwcrypt/find-all "Bitbucket")
-;; (pwcrypt/find-all "Costco")
+;; (pwcrypt-find-all "Bitbucket")
+;; (pwcrypt-find-all "Costco")
 
-(defun pwcrypt/find-first (name)
-  (first (pwcrypt/find-all name)))
+(defun pwcrypt-find-first (name)
+  (first (pwcrypt-find-all name)))
 
-(defun pwcrypt/get (loc)
+(defun pwcrypt-get (loc)
   (with-pwcrypt pwcrypt-file
     (let ((get1 (lambda (loc)
                   (reduce (lambda (m x)
@@ -41,54 +41,54 @@
           (mapcar (lambda (x) (funcall get1 x)) loc)
         (funcall get1 loc)))))
 
-;; (pwcrypt/get (first (pwcrypt/find-all "Hipchat")))
-;; (getf (pwcrypt/get (first (pwcrypt/find-all "Github"))) :username)
-;; (pwcrypt/get (pwcrypt/find-all "Github"))
+;; (pwcrypt-get (first (pwcrypt-find-all "Hipchat")))
+;; (getf (pwcrypt-get (first (pwcrypt-find-all "Github"))) :username)
+;; (pwcrypt-get (pwcrypt-find-all "Github"))
 
-(defun pwcrypt/index-extract-item ()
+(defun pwcrypt-index-extract-item ()
   (let ((pt (point)))
     (cons (buffer-substring-no-properties (+ 3 pt) (point-at-eol)) pt)))
 
-(defun pwcrypt/index ()
+(defun pwcrypt-index ()
   (with-pwcrypt pwcrypt-file
     (when (search-forward "* Logins")
-      (cdr (org-map-entries 'pwcrypt/index-extract-item t 'tree)))))
+      (cdr (org-map-entries 'pwcrypt-index-extract-item t 'tree)))))
 
-;; (pwcrypt/index)
-;; (pwcrypt/get (third (pwcrypt/index)))
+;; (pwcrypt-index)
+;; (pwcrypt-get (third (pwcrypt-index)))
 
-(defun pwcrypt/read-entry ()
+(defun pwcrypt-read-entry ()
   (let ((completion-ignore-case t))
     (list
      (completing-read "Entry: "
-                      (mapcar 'car (pwcrypt/index))))))
+                      (mapcar 'car (pwcrypt-index))))))
 
-(defun pwcrypt/get-property (key property)
-  (getf (pwcrypt/get (pwcrypt/find-first key)) property))
+(defun pwcrypt-get-property (key property)
+  (getf (pwcrypt-get (pwcrypt-find-first key)) property))
 
-(defun pwcrypt/copy-property (key property)
-  (let ((v (pwcrypt/get-property key property)))
+(defun pwcrypt-copy-property (key property)
+  (let ((v (pwcrypt-get-property key property)))
     (kill-new v)
     (funcall interprogram-cut-function v)))
 
-(defun pwcrypt/copy-username (arg)
-  (interactive (pwcrypt/read-entry))
-  (pwcrypt/copy-property arg :username))
+(defun pwcrypt-copy-username (arg)
+  (interactive (pwcrypt-read-entry))
+  (pwcrypt-copy-property arg :username))
 
-(defun pwcrypt/copy-password (arg)
-  (interactive (pwcrypt/read-entry))
-  (pwcrypt/copy-property arg :password))
+(defun pwcrypt-copy-password (arg)
+  (interactive (pwcrypt-read-entry))
+  (pwcrypt-copy-property arg :password))
 
-(defun pwcrypt/visit-url (arg)
-  (interactive (pwcrypt/read-entry))
-  (let ((url (pwcrypt/get-property arg :url)))
+(defun pwcrypt-visit-url (arg)
+  (interactive (pwcrypt-read-entry))
+  (let ((url (pwcrypt-get-property arg :url)))
     (if url
         (browse-url-default-browser url)
       (message "No url for %s" arg))))
 
-(defun pwcrypt/show-notes (arg)
-  (interactive (pwcrypt/read-entry))
-  (let ((notes (pwcrypt/get-property arg :notes))
+(defun pwcrypt-show-notes (arg)
+  (interactive (pwcrypt-read-entry))
+  (let ((notes (pwcrypt-get-property arg :notes))
         (buf (get-buffer-create "*pwcrypt-notes*")))
     (with-current-buffer buf
       (password-mode -1)
@@ -107,7 +107,7 @@
       (goto-char (point-max)))
     (pop-to-buffer buf)))
 
-(defun pwcrypt/encode-notes (p1 p2)
+(defun pwcrypt-encode-notes (p1 p2)
   (interactive "r")
   (goto-char p1)
   (while (search-forward "\n" p2 t)
