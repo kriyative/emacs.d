9e76a50df16d4b23d5721f35ff752ad6facac80b
Ram Krishnan <kriyative@gmail.com>
2015-08-08 11:59:59 -0700

    Fixed names to fit Emacs guidelines.

--- packages/ss.el
+++ packages/ss.el
@@ -13,7 +13,7 @@
 (mls-disk-start)
 (mls-misc-start)
 
-(defun ss/format-battery-stats (fmt)
+(defun ss-format-battery-stats (fmt)
   (let ((stats (mls-battery-fetch)))
     (mls-format-expand mls-battery-formatters
                        (if (stringp fmt)
@@ -21,21 +21,21 @@
                          (funcall fmt stats))
                        stats)))
 
-;; (ss/format-battery-stats "BAT[%p% %t %B]")
+;; (ss-format-battery-stats "BAT[%p% %t %B]")
 
-(defun ss/format-cpu-stats (fmt)
+(defun ss-format-cpu-stats (fmt)
   (let ((stats (mls-cpu-fetch)))
     (mls-format-expand mls-cpu-formatters fmt stats)))
 
-;; (ss/format-cpu-stats "%A%")
+;; (ss-format-cpu-stats "%A%")
 
-(defun ss/format-memory-stats (fmt)
+(defun ss-format-memory-stats (fmt)
   (let ((stats (mls-memory-fetch)))
     (mls-format-expand mls-memory-formatters fmt stats)))
 
-;; (ss/format-memory-stats "%R%")
+;; (ss-format-memory-stats "%R%")
 
-(defun ss/format-disk-stats (fmt)
+(defun ss-format-disk-stats (fmt)
   (let ((stats (mls-disk-fetch)))
     (mls-format-expand mls-disk-formatters fmt stats)))
 
@@ -57,7 +57,7 @@
                                            boot-time))))
     (list load boot-time emacs-uptime system-uptime)))
 
-(defun ss/format-misc-stats (fmt)
+(defun ss-format-misc-stats (fmt)
   (let ((stats (mls-misc-fetch)))
     (mls-format-expand mls-misc-formatters fmt stats)))
 
@@ -67,7 +67,7 @@
            (dbm-range (- 90.0 dbm-floor)))
       (min 1 (- 1 (/ (- (abs dbm) dbm-floor) dbm-range))))))
 
-(defun ss/format-wifi-stats ()
+(defun ss-format-wifi-stats ()
   (with-temp-buffer
     (call-process "iwconfig" nil t nil "wlan0")
     (goto-char (point-min))
@@ -79,45 +79,45 @@
           (format "NET[%s %ddBm %d%%]" essid dbm (* 100 (dbm-to-signal dbm)))
         "NET[N/A]"))))
 
-;; (ss/format-wifi-stats)
+;; (ss-format-wifi-stats)
 
-(defun ss/current ()
+(defun ss-current ()
   (mapconcat 'identity
              (list
               (concat (user-login-name) "@" (system-name))
-              (ss/format-battery-stats
+              (ss-format-battery-stats
                (lambda (stats)
                  (if (equal "AC" (car (nth 4 stats)))
                      "BAT[%p% AC]"
                    "BAT[%p% %t %B]")))
-              (ss/format-cpu-stats "CPU[%C0% %C1% %C2% %C3%]")
-              (ss/format-memory-stats "MEM[%R% %fMB]")
-              (ss/format-disk-stats "DISK[%p%]")
-              (ss/format-misc-stats "LOAD[%L1]")
-              (ss/format-wifi-stats)
+              (ss-format-cpu-stats "CPU[%C0% %C1% %C2% %C3%]")
+              (ss-format-memory-stats "MEM[%R% %fMB]")
+              (ss-format-disk-stats "DISK[%p%]")
+              (ss-format-misc-stats "LOAD[%L1]")
+              (ss-format-wifi-stats)
               (current-time-string))
              " "))
 
-;; (ss/current)
+;; (ss-current)
 
-(defun ss/info ()
+(defun ss-info ()
   (interactive)
-  (message "%s" (ss/current)))
+  (message "%s" (ss-current)))
 
-(defun ss/stat-ps (ps-cmd &optional num-rows)
+(defun ss-stat-ps (ps-cmd &optional num-rows)
   (save-excursion
     (shell-command ps-cmd t t))
   (forward-line (1+ (or num-rows 10)))
   (delete-region (point) (point-max))
   (newline 2))
 
-(defun ss/stat ()
+(defun ss-stat ()
   (interactive)
   (with-current-buffer (get-buffer-create "*ss-stat*")
-    (local-set-key "g" 'ss/stat)
+    (local-set-key "g" 'ss-stat)
     (delete-region (point-min) (point-max))
-    (ss/stat-ps "top -b -n 1" 20)
-    (ss/stat-ps "ps axk-pcpu -o pid,user,pcpu,cputime:10,comm")
-    (ss/stat-ps "ps axk-size -o pid,user,%mem,size:10,comm")))
+    (ss-stat-ps "top -b -n 1" 20)
+    (ss-stat-ps "ps axk-pcpu -o pid,user,pcpu,cputime:10,comm")
+    (ss-stat-ps "ps axk-size -o pid,user,%mem,size:10,comm")))
 
 (provide 'ss)
