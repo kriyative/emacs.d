f4d29cdfcf59508a99706fecef446a0590be201e
Ram Krishnan <kriyative@gmail.com>
2015-11-29 18:19:07 -0800

    Futzing with fonts again

--- bootstrap/window-x.el
+++ bootstrap/window-x.el
@@ -7,14 +7,38 @@
 ;;........1.........2.........3.........4.........5.........6.........7.........8.........9
 ;;23456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
 (defvar x-font nil)
-(setq x-font "Liberation Mono 9")
-;; (setq x-font "-unknown-DejaVu Sans Mono-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1")
-;; (setq x-font "-unknown-Liberation Mono-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1")
-(set-frame-font x-font t t)
-(setq default-frame-alist `((font . ,x-font)))
+(setq x-font "Liberation Mono 10")
+(let ((x-font
+       ;; "Andale Mono 11"
+       ;; "Consolas 10"
+       ;; "DejaVu Sans Mono Book 11"
+       ;; "Droid Sans Mono 10"
+       ;; "FreeMono 11"
+       ;; "Inconsolata 11"
+       ;; "Liberation Mono 10"
+       "Ubuntu Mono 12"
+       ))
+  ;; (setq x-font "-unknown-DejaVu Sans Mono-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1")
+  ;; (setq x-font "-unknown-Liberation Mono-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1")
+  (set-frame-font x-font t t)
+  (setq default-frame-alist `((font . ,x-font))))
+
+(defvar *emacs-focused-p* t)
+(defun emacs-focused-p ()
+  *emacs-focused-p*)
+
+(defun focus-in-hook ()
+  (setq *emacs-focused-p* t))
+
+(add-hook 'focus-in-hook 'focus-in-hook)
+
+(defun focus-out-hook ()
+  (setq *emacs-focused-p* nil))
+
+(add-hook 'focus-out-hook 'focus-out-hook)
 
 (defun x-notify (message &optional title)
-  (let ((alert-default-style 'notifications))
+  (let ((alert-default-style (if (emacs-focused-p) 'message 'notifications)))
     (alert message :title (or title (concat "emacs@" system-name)))))
 
 ;; (x-notify "hello")
@@ -24,4 +48,4 @@
 
 (push 'compilation-end-notifier compilation-finish-functions)
 
-(mouse-avoidance-mode 'banish)
+(mouse-avoidance-mode 'none)
