5735cd1051f37c3d42d3ca8e69dba9b05a85182b
Ram Krishnan <kriyative@gmail.com>
2016-04-24 19:55:02 -0700

    Added `dired-open-file` and keybinding

--- bootstrap/plat-linux.el
+++ bootstrap/plat-linux.el
@@ -15,3 +15,10 @@
 (require 'org)
 (require 'org-passwords)
 
+(defun dired-open-file ()
+  "In dired, open the file named on this line."
+  (interactive)
+  (let* ((file (dired-get-filename nil t)))
+    (call-process "xdg-open" nil 0 nil file)))
+
+(define-key dired-mode-map (kbd "C-c o") 'dired-open-file)
