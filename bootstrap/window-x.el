c14a8ae2d8ddb00a7265384edc67b096c418fd38
Ram Krishnan <kriyative@gmail.com>
2016-06-09 23:26:03 -0700

    Derive font size from display-dpi

--- bootstrap/window-x.el
+++ bootstrap/window-x.el
@@ -7,18 +7,32 @@
 ;;........1.........2.........3.........4.........5.........6.........7.........8.........9
 ;;23456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
 (defvar x-font nil)
-(setq x-font "Liberation Mono 10")
-(let ((x-font
-       ;; "Consolas 12"
+;; (setq x-font "Liberation Mono 10")
+
+(defun display-dpi ()
+  (let* ((monitor-attribs (display-monitor-attributes-list))
+         (attrib1 (first monitor-attribs))
+         (mm-size (cdr (assq 'mm-size attrib1)))
+         (width-inches (* (first mm-size) 0.039)))
+    (round (/ (x-display-pixel-width) width-inches))))
+
+;; (display-dpi)
+
+(defun optimum-font-size ()
+  (if (< 90 (display-dpi)) 14 11))
+
+(let* ((font-family "Consolas")
+       (font-size (optimum-font-size))
+       (x-font (concat font-family " " (prin1-to-string font-size)))
+       ;; "Consolas 10"
+       ;; "Consolas 14"
        ;; "DejaVu Sans Mono Book 10"
        ;; "Droid Sans Mono 10"
        ;; "FreeMono 11"
        ;; "Inconsolata 11"
-       ;; "Liberation Mono 10"
-       "Ubuntu Mono 12"
-       ))
-  ;; (setq x-font "-unknown-DejaVu Sans Mono-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1")
-  ;; (setq x-font "-unknown-Liberation Mono-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1")
+       ;; "Liberation Mono 13"
+       ;;  "Ubuntu Mono 14"
+       )
   (set-frame-font x-font t t)
   (setq default-frame-alist `((font . ,x-font))))
 
