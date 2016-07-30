eda6daf4648c2af3d9c6504dcd6debc0118249d6
Ram Krishnan <kriyative@gmail.com>
2016-07-30 10:57:18 -0700

    mu4e customizations

--- lisp/setup.el
+++ lisp/setup.el
@@ -384,20 +384,22 @@
 
 ;;;;;;;;;;;;;;;; mail ;;;;;;;;;;;;;;;;
 
+(defun mu4e-headers-mode-hook ()
+  (setq mu4e-headers-visible-columns (/ (frame-width) 2)))
+
 (defun setup-mu4e ()
   (setq mu4e-maildir       "~/Mail" ;; top-level Maildir
-        mu4e-get-mail-command "offlineimap -o"
+        mu4e-get-mail-command "offlineimap"
         mu4e-view-prefer-html t
         mu4e-hide-index-messages t
         mu4e-split-view 'vertical
-        mu4e-headers-visible-columns (/ (frame-width) 2)
         mu4e-headers-fields '((:human-date .  12) ;; alternatively, use :human-date
                               ;; (:flags      .   6)
                               (:from       .  16)
-                              (:subject    .  nil))))
+                              (:subject    .  nil)))
+  (add-hook 'mu4e-headers-mode-hook 'mu4e-headers-mode-hook))
 
 (use-package mu4e :config (setup-mu4e))
-
 (use-package mu4e-multi :config (mu4e-multi-enable))
 (use-package mu4e-maildir :config (mu4e-maildirs-extension))
 
