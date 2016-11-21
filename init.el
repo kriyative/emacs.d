c906468bf989bd1c9db3195c0f9b3cac6505d6ee
Ram Krishnan <kriyative@gmail.com>
2016-11-21 07:11:21 -0800

    Moved customization settings to custom.el

--- init.el
+++ init.el
@@ -9,28 +9,4 @@
 (load "window")
 (load "commands")
 (load "post")
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(auth-source-save-behavior nil)
- '(smtpmail-smtp-server "smtp.gmail.com" t)
- '(smtpmail-smtp-service 25 t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(erc-nick-default-face ((t (:foreground "blue" :weight bold))))
- '(erc-timestamp-face ((t (:foreground "blue" :weight bold))))
- '(fringe ((t nil)))
- '(jabber-chat-prompt-foreign ((t (:weight bold))))
- '(jabber-chat-prompt-local ((t (:foreground "blue"))))
- '(jabber-chat-prompt-system ((t (:foreground "red"))))
- '(jabber-title-medium ((t (:height 1.5))))
- '(mu4e-header-highlight-face ((t (:background "grey" :underline t :weight bold))))
- '(mu4e-unread-face ((t (:inherit font-lock-keyword-face :foreground "blue" :weight bold)))))
-(put 'narrow-to-region 'disabled nil)
-(put 'scroll-left 'disabled nil)
+(load custom-settings-file 'noerror)
