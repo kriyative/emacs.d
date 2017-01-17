35db5ec5f8584d92c495e13871e7b43260b70e18
Ram Krishnan <kriyative@gmail.com>
2017-01-17 08:14:33 -0800

    Override plantutml display to pop up image buffer if it exists

--- lisp/post.el
+++ lisp/post.el
@@ -2,3 +2,16 @@
 
 (when org-agenda-files
   (add-hook 'display-time-hook 'show-daily-agenda))
+
+;;; overrides
+;;;
+(defun plantuml-display-image()
+  "Display the rendered image"
+  (interactive)
+  (let* ((plantuml-file (concat (file-name-sans-extension buffer-file-name) ".png"))
+         (plantuml-buf (get-buffer (file-name-nondirectory plantuml-file))))
+    (if (not (buffer-live-p plantuml-buf))
+	(find-file plantuml-file)
+      (progn
+	(pop-to-buffer plantuml-buf)
+	(revert-buffer nil t nil)))))
