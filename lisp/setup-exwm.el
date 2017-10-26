24bfb30a2f5e012e915208d435dcd46073c81b1d
Ram Krishnan <kriyative@gmail.com>
2017-10-25 22:02:24 -0700

    Updated exwm customizations
    - added `exec!` and `spawn&`
    - added `\M-w` and `\C-y` simulation keys
    - added `lock-screen`
    - added `highlight-focus`

--- lisp/setup-exwm.el
+++ lisp/setup-exwm.el
@@ -1,7 +1,7 @@
 (el-get-bundle ch11ng/xelb)
 (el-get-bundle ch11ng/exwm)
 
-(defun exec-process (command)
+(defun exec! (command)
   (interactive (list (read-shell-command "$ ")))
   (let* ((program-and-args (split-string command))
          (program (car program-and-args))
@@ -11,12 +11,33 @@
       (insert "$ " command "\n")
       (apply 'call-process program nil t t args))))
 
+(defun spawn& (command)
+  (interactive (list (read-shell-command "$ ")))
+  (let* ((program-and-args (split-string command))
+         (program (car program-and-args))
+         (program-name (file-name-nondirectory program))
+         (args (cdr program-and-args)))
+    (apply 'start-process program-name program-name program args)))
+
+(global-set-key (kbd "s-!") 'exec!)
+
 (defun my-exwm-init ()
-  (start-process
-   "gnome-settings-daemon"
-   "gnome-settings-daemon"
-   "/usr/lib/gnome-settings-daemon/gsd-xsettings")
-  (exec-process (concat
+  (exwm-input-set-simulation-keys
+   '(([?\C-b] . left)
+     ([?\C-f] . right)
+     ([?\C-p] . up)
+     ([?\C-n] . down)
+     ([?\C-a] . home)
+     ([?\C-e] . end)
+     ([?\M-v] . prior)
+     ([?\C-v] . next)
+     ([?\C-d] . delete)
+     ([?\C-k] . (S-end delete))
+     ([?\M-w] . ?\C-c)
+     ;; ([?\C-w] . ?\C-x)
+     ([?\C-y] . ?\C-v)))
+  (spawn& "/usr/lib/gnome-settings-daemon/gsd-xsettings")
+  (exec! (concat
                  "synclient"
                  " VertTwoFingerScroll=1"
                  " VertScrollDelta=-111"
@@ -26,24 +47,41 @@
                  " TapButton2=3"
                  " TapButton3=2"
                  " PalmDetect=1"))
-  (exec-process "setxkbmap -option ctrl:nocaps")
-  (exec-process "xsetroot -cursor_name left_ptr -bg black"))
+  (exec! "setxkbmap -option ctrl:nocaps")
+  (exec! "xsetroot -cursor_name left_ptr -bg black")
+  (spawn& "xscreensaver -no-splash"))
+
+(defun lock-screen ()
+  (interactive)
+  (exec! "xscreensaver-command -lock"))
+
+(global-set-key (kbd "s-l") 'lock-screen)
 
 (use-package exwm
   :config
   (display-time))
+
 (use-package exwm-config
   :config
   (setq exwm-workspace-number 4)
-  (add-hook 'exwm-floating-setup-hook #'exwm-layout-hide-mode-line)
-  (add-hook 'exwm-floating-exit-hook #'exwm-layout-show-mode-line)
+  ;; (add-hook 'exwm-floating-setup-hook #'exwm-layout-hide-mode-line)
+  ;; (add-hook 'exwm-floating-exit-hook #'exwm-layout-show-mode-line)
   (exwm-config-default)
   (my-exwm-init))
-(use-package exwm-cm
-  :config
-  (exwm-cm-enable))
+
+;; (use-package exwm-cm
+;;   :config
+;;   (exwm-cm-enable))
+
 (use-package exwm-randr
   :config (exwm-randr-enable))
+
+(el-get-bundle kriyative/highlight-focus)
+(use-package highlight-focus
+  :config
+  (setq highlight-focus:face 'mode-line
+        highlight-focus:face-property :background
+        highlight-focus:face-property-value "darkgreen"))
 
 (global-set-key (kbd "<XF86AudioPlay>") 'emms-pause)
 (global-set-key (kbd "<XF86AudioStop>") 'emms-stop)
@@ -56,3 +94,6 @@
 (global-set-key (kbd "<XF86AudioLowerVolume>") 'pulseaudio-control-decrease-volume)
 (global-set-key (kbd "<XF86AudioRaiseVolume>") 'pulseaudio-control-increase-volume)
 (pulseaudio-control-default-keybindings)
+
+(define-key exwm-mode-map (kbd "s-q") #'exwm-input-send-next-key)
+(define-key exwm-mode-map (kbd "s-S-q") #'exwm-input-send-next-key)
