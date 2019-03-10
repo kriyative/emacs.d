6f7aec72a9f063aba34c526274bdd200196fdd9a
Ram Krishnan <kriyative@gmail.com>
2019-03-10 07:59:46 -0700

    Enables startup level 5 only on specific systems

--- init.el
+++ init.el
@@ -1,8 +1,12 @@
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp"))
 (load "base")
-
 (load-file-if-exists "~/.personal.el")
-
-(startup-emacs 5)
+(startup-emacs
+ (cond
+  ((and window-system
+	(string-equal "luna" system-name))
+   5)
+  (window-system 4)))
 (load "plat")
 (load "window")
+(load-file-if-exists custom-file 'noerror)
