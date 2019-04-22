c7ea407e3d40a18eeed572ac032bf84dfa016bae
Ram Krishnan <kriyative@gmail.com>
2019-04-22 07:41:52 -0700

    Adds utf-8 coding customizations

--- startup/00.el
+++ startup/00.el
@@ -81,10 +81,12 @@
 ;;;;;;;;;;;;;;;; charset encoding ;;;;;;;;;;;;;;;;
 
 (prefer-coding-system       'utf-8)
+(set-file-name-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
-(setq default-buffer-file-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING))
 
 (put 'narrow-to-page 'disabled nil)
 (put 'erase-buffer 'disabled nil)
