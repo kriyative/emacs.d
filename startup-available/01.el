fc7b83b3ff393448d2dc144423915cc4e1cfd71e
Ram Krishnan <kriyative@gmail.com>
2019-11-06 10:57:18 -0800

    Adds w3m config

--- startup-available/01.el
+++ startup-available/01.el
@@ -205,11 +205,27 @@
   (slime-list-connections)
   (pop-to-buffer "*SLIME Connections*"))
 
+(defun w3m-browse-url-other-window (url &optional new-session)
+  (save-excursion
+    (when (one-window-p)
+      (split-window-horizontally))
+    (other-window 1)
+    (let ((w3m-use-tab nil))
+      (w3m-browse-url url new-session))))
+
 (defun my-url-browser-function (&rest args)
   (apply (if current-prefix-arg
 	     'browse-url-default-browser
 	   'w3m-browse-url-other-window)
 	 args))
+
+(defun w3m-mode-hook ()
+  (define-key w3m-mode-map "\M-t" 'w3m-copy-buffer))
+
+(use-package w3m
+  :config
+  (add-hook 'w3m-mode-hook 'w3m-mode-hook)
+  (setq browse-url-browser-function 'w3m-browse-url-other-window))
 
 (defun my-slime-mode-hook ()
   (setq browse-url-browser-function 'my-url-browser-function)
