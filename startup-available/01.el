cea841d886b8bb29f2313d9c8718d82ab2c9cac8
Ram Krishnan <kriyative@gmail.com>
2020-02-19 14:46:35 -0800

    Refactors w3m to its own startup

--- startup-available/01.el
+++ startup-available/01.el
@@ -1,7 +1,6 @@
 (rk-el-get-bundles
  diminish
  dired-hacks
- emacs-w3m
  geiser
  gh
  graphviz-dot-mode
@@ -197,28 +196,6 @@
   (interactive)
   (slime-list-connections)
   (pop-to-buffer "*SLIME Connections*"))
-
-(defun w3m-browse-url-other-window (url &optional new-session)
-  (save-excursion
-    (when (one-window-p)
-      (split-window-horizontally))
-    (other-window 1)
-    (let ((w3m-use-tab nil))
-      (w3m-browse-url url new-session))))
-
-(defun rk-url-browser-function (&rest args)
-  (apply (if current-prefix-arg
-             'browse-url-default-browser
-           'w3m-browse-url-other-window)
-         args))
-
-(defun w3m-mode-hook ()
-  (define-key w3m-mode-map "\M-t" 'w3m-copy-buffer))
-
-(use-package w3m
-  :config
-  (add-hook 'w3m-mode-hook 'w3m-mode-hook)
-  (setq browse-url-browser-function 'w3m-browse-url-other-window))
 
 (defun rk-slime-mode-hook ()
   (setq browse-url-browser-function 'rk-url-browser-function)
---
+++ startup-available/05-w3m.el
@@ -0,0 +1,23 @@
+(rk-el-get-bundles emacs-w3m)
+
+(defun w3m-browse-url-other-window (url &optional new-session)
+  (save-excursion
+    (when (one-window-p)
+      (split-window-horizontally))
+    (other-window 1)
+    (let ((w3m-use-tab nil))
+      (w3m-browse-url url new-session))))
+
+(defun rk-url-browser-function (&rest args)
+  (apply (if current-prefix-arg
+             'browse-url-default-browser
+           'w3m-browse-url-other-window)
+         args))
+
+(defun w3m-mode-hook ()
+  (define-key w3m-mode-map "\M-t" 'w3m-copy-buffer))
+
+(use-package w3m
+  :config
+  (add-hook 'w3m-mode-hook 'w3m-mode-hook)
+  (setq browse-url-browser-function 'w3m-browse-url-other-window))
