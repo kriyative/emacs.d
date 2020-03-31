4a2be4a1e556936297c4cd29e42f7dc1f10b79f6
Ram Krishnan <kriyative@gmail.com>
2020-03-30 20:11:09 -0700

    Some jabber face customizations

--- custom.el
+++ custom.el
@@ -8,26 +8,22 @@
  '(fringe-mode 4 nil (fringe))
  '(menu-bar-mode nil)
  '(package-selected-packages
-   (quote
-    (speed-type dired-du oauth2 slack queue htmlize csv-mode clojure-mode adaptive-wrap)))
+   '(tracking speed-type dired-du oauth2 slack queue htmlize csv-mode clojure-mode adaptive-wrap))
  '(safe-local-variable-values
-   (quote
-    ((eval ignore-errors "Write-contents-functions is a buffer-local alternative to before-save-hook"
-           (add-hook
-            (quote write-contents-functions)
-            (lambda nil
-              (delete-trailing-whitespace)
-              nil))
-           (require
-            (quote whitespace))
+   '((eval ignore-errors "Write-contents-functions is a buffer-local alternative to before-save-hook"
+           (add-hook 'write-contents-functions
+                     (lambda nil
+                       (delete-trailing-whitespace)
+                       nil))
+           (require 'whitespace)
            "Sometimes the mode needs to be toggled off and on."
            (whitespace-mode 0)
            (whitespace-mode 1))
      (whitespace-style face tabs trailing lines-tail)
      (cider-shadow-cljs-default-options . "browser")
      (cider-default-cljs-repl . shadow)
-     (checkdoc-package-keywords-flag))))
- '(send-mail-function (quote smtpmail-send-it))
+     (checkdoc-package-keywords-flag)))
+ '(send-mail-function 'smtpmail-send-it)
  '(smtpmail-smtp-server "smtp.gmail.com")
  '(smtpmail-smtp-service 25)
  '(tool-bar-mode nil)
@@ -47,7 +43,9 @@
  '(jabber-chat-prompt-foreign ((t (:weight bold))))
  '(jabber-chat-prompt-local ((t (:foreground "blue"))))
  '(jabber-chat-prompt-system ((t (:foreground "red"))))
- '(jabber-title-medium ((t (:height 1.5))))
+ '(jabber-title-large ((t (:height 1.5))))
+ '(jabber-title-medium ((t (:weight bold))))
+ '(jabber-title-small ((t (:height 1.0))))
  '(magit-tag ((t (:background "yellow" :foreground "black"))))
  '(mu4e-header-highlight-face ((t (:background "grey" :underline t :weight bold))))
  '(mu4e-unread-face ((t (:inherit font-lock-keyword-face :foreground "blue" :weight bold)))))
