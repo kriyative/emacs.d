dece0a1fbc6e2c2acd5a4e3f282b73e6cdf2d9ac
Ram Krishnan <kriyative@gmail.com>
2020-04-23 11:10:08 -0700

    More fixes to code-body printing

--- startup-available/05-jabber.el
+++ startup-available/05-jabber.el
@@ -14,7 +14,7 @@
 
 (defun rk--split-sections (body)
   (let ((cursor 0) sections slug)
-    (while (setf slug (string-match "```\\([^ \t\n]*\\)" body cursor))
+    (while (setf slug (and body (string-match "```\\([^ \t\n]*\\)" body cursor)))
       (when (< 0 slug)
         (push (make-rk--text-section :type 'plain
                                      :text (substring body cursor slug))
@@ -35,31 +35,32 @@
             sections))
     (nreverse sections)))
 
-(defun rk--jabber-chat-print-code-body (xml-data who mode)
+(defun rk--jabber-chat-code-body (xml-data who mode)
   "Print body for received message in XML-DATA."
   (let* ((body (car
 	        (jabber-xml-node-children
 		 (car
 		  (jabber-xml-get-children xml-data 'body)))))
          (sections (rk--split-sections body)))
-    (if sections
-        (when (eql mode :insert)
-          (dolist (section sections)
-            (if (eq 'code (rk--text-section-type section))
-                (insert (rk--text-section-text section))
-              (insert (jabber-propertize
-		       (rk--text-section-text section)
-		       'face (case who
-			       ((:foreign :muc-foreign) 'jabber-chat-text-foreign)
-			       ((:local :muc-local) 'jabber-chat-text-local)))))))
-      (jabber-chat-print-body xml-data who mode))
-    t))
+    (when body
+      (if sections
+          (when (eql mode :insert)
+            (dolist (section sections)
+              (if (eq 'code (rk--text-section-type section))
+                  (insert (rk--text-section-text section))
+                (insert (jabber-propertize
+		         (rk--text-section-text section)
+		         'face (case who
+			         ((:foreign :muc-foreign) 'jabber-chat-text-foreign)
+			         ((:local :muc-local) 'jabber-chat-text-local)))))))
+        (jabber-chat-normal-body xml-data who mode))
+      t)))
 
 (use-package jabber
   :config
   (add-hook 'jabber-chat-mode-hook 'turn-on-visual-line-mode)
-  (remove-hook 'jabber-chat-printers 'jabber-chat-print-body)
-  (add-hook 'jabber-chat-printers 'rk--jabber-chat-print-code-body)
+  (remove-hook 'jabber-body-printers 'jabber-chat-normal-body)
+  (add-hook 'jabber-body-printers 'rk--jabber-chat-code-body)
   (setq jabber-auto-reconnect t
         jabber-vcard-avatars-retrieve nil
         jabber-avatar-verbose nil
@@ -74,7 +75,9 @@
         jabber-chat-local-prompt-format "[%t] %n> "
         jabber-history-size-limit 1024
         jabber-console-truncate-lines 1000
-        jabber-show-resources nil)
+        jabber-show-resources nil
+        alert-default-style 'notifications)
+  (add-hook 'jabber-alert-message-hooks 'jabber-message-alert)
   :bind (:map jabber-chat-mode-map
               ([C-return] . ffap))
   :bind ("C-x C-j" . jabber-global-keymap))
