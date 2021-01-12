e1a10bcdc31cb8cbae3a7773f4b6cdabde904298
Ram Krishnan <kriyative@gmail.com>
2021-01-11 19:28:04 -0800

    Adds minimal matrix notification hook

--- startup-available/06-matrix-chat.el
+++ startup-available/06-matrix-chat.el
@@ -14,13 +14,21 @@
 
 (defun rk--matrix-client-room-list-mode-hook ()
   (local-set-key (kbd "<return>") 'rk--matrix-client-open-room-window))
+
 (defun rk--matrix-client-mode-hook ()
   (local-set-key (kbd "C-<return>") 'ffap))
+
+(defun rk--matrix-client-notify-hook (event-type data rest)
+  (notifications-notify :title "emacs-matrix"
+                        :body (alist-get 'body (alist-get 'content data))
+                        :app-name "Emacs"))
 
 (use-package matrix-client
   :config
   (add-hook 'matrix-client-room-list-mode-hook
             'rk--matrix-client-room-list-mode-hook)
   (add-hook 'matrix-client-mode-hook 'rk--matrix-client-mode-hook)
+  (add-hook 'matrix-client-notify-hook 'rk--matrix-client-notify-hook)
   (setq matrix-client-save-token t
-        matrix-client-save-token-file "~/.emacs.d/matrix-client.el.token"))
+        matrix-client-save-token-file "~/.emacs.d/matrix-client.el.token"
+        matrix-client-room-send-as-org-by-default t))
