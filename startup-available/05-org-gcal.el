75756ccc4b1f7de331de2363063dad277140b22e
Ram Krishnan <kriyative@gmail.com>
2021-10-12 11:08:38 -0700

    Hack to support multiple gcal account syncing

--- startup-available/05-org-gcal.el
+++ startup-available/05-org-gcal.el
@@ -1,5 +1,4 @@
-(rk-el-get-bundles persist
-                   org-gcal)
+(rk-el-get-bundles persist org-gcal)
 
 (use-package org-gcal
   :after org
@@ -19,24 +18,28 @@
                               file-alist
                               token-file)
         account
-      (let ((buf (get-buffer-create (concat "org-gcal: " name))))
-        (with-current-buffer buf
-          (make-local-variable 'org-gcal-client-id)
-          (make-local-variable 'org-gcal-client-secret)
-          (make-local-variable 'org-gcal-file-alist)
-          (make-local-variable 'org-gcal-token-file)
-          (setq org-gcal-client-id client-id
-                org-gcal-client-secret client-secret
-                org-gcal-file-alist file-alist
-                org-gcal-token-file token-file)
-          (org-gcal-fetch))))))
+      (dolist (cal-id-file file-alist)
+        (let ((cal-id (car cal-id-file))
+              (file (cdr cal-id-file)))
+          (with-current-buffer (find-file-noselect file)
+            (make-local-variable 'org-gcal-client-id)
+            (make-local-variable 'org-gcal-client-secret)
+            (make-local-variable 'org-gcal-fetch-file-alist)
+            (make-local-variable 'org-gcal-token-file)
+            (make-local-variable 'org-gcal-token-plist)
+            (setq org-gcal-client-id client-id
+                  org-gcal-client-secret client-secret
+                  org-gcal-fetch-file-alist file-alist
+                  org-gcal-token-file token-file))))
+      (let ((org-gcal-token-file token-file))
+        (org-gcal-fetch)))))
 
 (defvar rk--org-gcal-accounts nil)
 
 (defun rk-org-gcal-multi-fetch ()
   (interactive)
   (let ((browse-url-browser-function 'browse-url-chromium))
-   (rk--org-gcal-multi-fetch rk--org-gcal-accounts)))
+    (rk--org-gcal-multi-fetch rk--org-gcal-accounts)))
 
 (defun rk--org-gcal-files ()
   (reduce (lambda (all-files account)
