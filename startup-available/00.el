1debb2a4ad3890459f5147ea8dd1d1b5a4764cef
Ram Krishnan <kriyative@gmail.com>
2022-01-03 05:43:16 -0800

    Refactors rk-el-get-bundles

--- startup-available/00.el
+++ startup-available/00.el
@@ -9,11 +9,10 @@
       (url-retrieve-synchronously
        "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
     (goto-char (point-max))
-    (eval-print-last-sexp)))
+    (eval-print-last-sexp)
+    (require 'el-get)))
 
 (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
-
-(require 'el-get)
 
 (defmacro rk-el-get-bundles (&rest rcps)
   `(progn
