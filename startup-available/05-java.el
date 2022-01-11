91a4c839ee66355e18cc9725eb14462e49924416
Ram Krishnan <kriyative@gmail.com>
2022-01-11 09:09:16 -0800

    Refactors lsp and lsp-java into separate startup files

--- startup-available/05-java.el
+++ startup-available/05-java.el
@@ -1,17 +1,7 @@
-(rk-require-packages lsp-mode
-		     lsp-ui
-		     treemacs
-		     lsp-treemacs
-		     lsp-java)
-
-(use-package lsp-mode
-  :hook
-  ((lsp-mode . lsp-enable-which-key-integration))
-  :config
-  (setq lsp-completion-enable-additional-text-edit nil))
-
-(use-package treemacs)
+(rk-require-packages lsp-java)
 
 (use-package lsp-java
   :config
   (add-hook 'java-mode-hook #'lsp))
+
+(use-package dap-java :ensure nil)
