3d122cc8faad2562e3e85913382326a96acf25b2
Ram Krishnan <kriyative@gmail.com>
2022-08-27 19:03:48 -0700

    Refactors init, adds private/ folder

--- init.el
+++ init.el
@@ -1,3 +1,6 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; bootstrap straight.el
+
 (defvar bootstrap-version)
 (let ((bootstrap-file (expand-file-name "straight/repos/straight.el/bootstrap.el"
                                         user-emacs-directory))
@@ -5,7 +8,8 @@
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
         (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         (concat "https://raw.githubusercontent.com"
+                 "/radian-software/straight.el/develop/install.el")
          'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
@@ -13,18 +17,43 @@
 
 (straight-use-package 'use-package)
 
-(require 'cl)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; load standard customizations
 
 (load custom-file nil t)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; some helper functions
+
+(defun load-if-present (filename &rest load-args)
+  "Load FILENAME if it exists on disk using `load' passing on
+LOAD-ARGS"
+  (when (file-exists-p filename)
+    (apply 'load filename load-args)))
+
 (defvar rk-startup-modules '("base" "display" "org" "commands"))
 
-(load (concat "~/." (system-name) ".el") nil t)
-(load "~/.personal.el.gpg" nil t)
+(defun rk-load-startup-modules ()
+  (dolist (module rk-startup-modules)
+    (load
+     (expand-file-name (concat "startup/" module ".el")
+                       user-emacs-directory))))
 
-(dolist (module rk-startup-modules)
-  (message "Loading %s" module)
-  (load (expand-file-name (concat "startup/" module ".el")
-                          user-emacs-directory)
-        nil
-        t))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; load machine specific customizations if present
+
+(load-if-present
+ (expand-file-name (concat "private/" (system-name) ".el")
+                   user-emacs-directory))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; load private (potential secrets) customizations if present
+
+(load-if-present
+ (expand-file-name "private/.personal.el.gpg"
+                   user-emacs-directory))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; load customization modules
+
+(rk-load-startup-modules)
