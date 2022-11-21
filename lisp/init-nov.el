76549d0d5e60421360a38b43a5238f5cd900e6e4
Ram Krishnan <kriyative@gmail.com>
2022-11-21 06:56:51 -0800

    nov.el mode customizations

--- lisp/init-nov.el
+++ lisp/init-nov.el
@@ -3,6 +3,8 @@
 (use-package nov
   :straight t
   :config
-  (add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode)))
+  (add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode))
+  (setq nov-text-width 70
+        nov-variable-pitch nil))
 
 (provide 'init-nov)
