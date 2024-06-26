cca209926b6f7d9c06e25d4ca5f6a433397af123
Ram Krishnan <rk@kriyative.net>
2024-06-25 18:57:42 -0700

    Apply post frame creation customizations

--- lisp/init-display.el
+++ lisp/init-display.el
@@ -292,14 +292,21 @@
 (menu-bar-mode -1)
 (set-default 'fringes-outside-margins t)
 
-(setq utf-translate-cjk-mode nil) ; disable CJK coding/encoding (Chinese/Japanese/Korean characters)
-(set-language-environment 'utf-8)
-(set-keyboard-coding-system 'utf-8-mac) ; For old Carbon emacs on OS X only
-(setq locale-coding-system 'utf-8)
-(set-default-coding-systems 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-selection-coding-system 'utf-8)
-(prefer-coding-system 'utf-8)
+(defun rk--setup-frame (frame)
+  ;; disable CJK coding/encoding (Chinese/Japanese/Korean characters)
+  (setq utf-translate-cjk-mode nil)
+  (set-language-environment 'utf-8)
+  ;; For old Carbon emacs on OS X only
+  (set-keyboard-coding-system 'utf-8-mac)
+  (setq locale-coding-system 'utf-8)
+  (set-default-coding-systems 'utf-8)
+  (set-terminal-coding-system 'utf-8)
+  (set-selection-coding-system 'utf-8)
+  (prefer-coding-system 'utf-8)
+  (setq visible-cursor nil)
+  (message "Applied new frame customizations"))
+
+(add-to-list 'after-make-frame-functions 'rk--setup-frame)
 
 (blink-cursor-mode -1)
 (when (fboundp 'tool-bar-mode)
